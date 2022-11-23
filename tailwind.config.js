841ddb85b0feddb078ab8893933da528e7a8b997
dad-sec <secdad1@gmail.com>
2022-11-23 21:29:04 +0100

    updated website info

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,16 @@ module.exports = {
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
+        blue: '#0014FF',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGreyishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)'
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        veryLightBlue: 'rgb(59 130 246)',
+        layer: '#E6CBA8'
       }
     },
   },
