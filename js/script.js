9964d8a97ad19c9be708406d32c74c780598b0b0
dad-sec <secdad1@gmail.com>
2022-11-24 03:27:44 +0100

    updated styleshhet error

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
-alert('Welcome to Garlicpay 🤝')
+//alert('Welcome to Garlicpay 🤝')
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
